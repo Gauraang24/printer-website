ae847d0cab77d81593c8e0072055fb32daa74a09
gaurang.more <gaurang.more@sunday.tech>
2024-09-22 20:51:42 +0530

    Product Page : ui initiated

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,11 +1,64 @@
+import { Segmented } from "antd";
+import { useEffect, useState } from "react";
+import { cartridgeCollection, printersCollection } from "../../staticData/productData";
 
-
+const constants = {
+    PRINTER: "1",
+    CARTRIDGE: "2",
+};
 const Product = () => {
-    return (
-        <div>
-            Product Page
-        </div>
-    )
-}
+    const [activeTab, setActiveTab] = useState(constants.PRINTER)
+    const [productData, setProductData] = useState(printersCollection)
 
-export default Product
+    useEffect(() => {
+        if (activeTab === constants.PRINTER) {
+            setProductData(printersCollection)
+        } else {
+            setProductData(cartridgeCollection)
+        }
+    }, [activeTab])
+
+    const options = [
+        {
+            label: "Printer",
+            value: constants.PRINTER,
+        },
+        {
+            label: "Cartridge",
+            value: constants.CARTRIDGE,
+        },
+    ];
+    return (
+        <section>
+            {/* export const printersCollection = [ */}
+            {/* Banner Section */}
+            <div className="w-full">
+                <img
+                    src="images/productPage/Banner.jpg"
+                    alt="Banner"
+                    className="w-full h-auto"
+                />
+            </div>
+
+            {/* ProductSection */}
+
+            <div className="w-full py-[63px]">
+                <div className="w-max mx-auto">
+                    <Segmented
+                        options={options}
+                        onChange={(value) => {
+                            setActiveTab(value)
+                        }}
+                    />
+                </div>
+            </div>
+            <div>
+                {productData.map((i) => {
+                    return <div key={i.key}></div>
+                })}
+            </div>
+        </section>
+    );
+};
+
+export default Product;
---
+++ src/staticData/productData.js
@@ -0,0 +1,95 @@
+export const printersCollection = [
+    {
+        heading: "CWC Multifunction Laser Printer Series",
+        data: [{
+            title: "CWC M5040DN/W",
+            link: "images/productPage/CWC_M1.png",
+            key: "M1",
+        },
+        {
+            title: "CWC M5030DN/W",
+            link: "images/productPage/CWC_M2.png",
+            key: "M2",
+        },
+        {
+            title: "CWC M2010DN/W",
+            link: "images/productPage/CWC_M3.png",
+            key: "M3",
+        },
+        {
+            title: "CWC MB5020N/W",
+            link: "images/productPage/CWC_M4.png",
+            key: "M4",
+        },
+        {
+            title: "CWC M1005N/W",
+            link: "images/productPage/CWC_M5.png",
+            key: "M5",
+        },]
+    },
+    {
+        heading: "CWC Laser Printer Series",
+        data: [{
+            title: "CWC P5040DN/W",
+            link: "images/productPage/CWC_P1.png",
+            key: "P1",
+        },
+        {
+            title: "CWC P5030DN/W",
+            link: "images/productPage/CWC_P2.png",
+            key: "P2",
+        },
+        {
+            title: "CWC P5010DN/W",
+            link: "images/productPage/CWC_P3.png",
+            key: "P3",
+        },
+        {
+            title: "CWC PB5040DN/W",
+            link: "images/productPage/CWC_P4.png",
+            key: "P4",
+        },
+        {
+            title: "CWC P5040",
+            link: "images/productPage/CWC_P5.png",
+            key: "P5",
+        },
+        {
+            title: "CWC 1020N/W",
+            link: "images/productPage/CWC_P6.png",
+            key: "P6",
+        },
+        ]
+    },
+];
+
+export const cartridgeCollection = [
+    {
+        heading: "Cartridges",
+        data: [{
+            title: "CWC TW502S",
+            link: "images/productPage/Cartridge_T1.png",
+            key: "T1",
+        },
+        {
+            title: "CWC TE201L",
+            link: "images/productPage/Cartridge_T2.png",
+            key: "T2",
+        },
+        {
+            title: "CWC TE503S",
+            link: "images/productPage/Cartridge_T3.png",
+            key: "T3",
+        },
+        {
+            title: "CWC TE504S",
+            link: "images/productPage/Cartridge_T4.png",
+            key: "T4",
+        },
+        {
+            title: "CWC 102-2K",
+            link: "images/productPage/Cartridge_T5.png",
+            key: "T5",
+        },]
+    },
+]
