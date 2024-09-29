2e7ffd16afa34404d04d94d0ce1c634bd6ec05d6
gaurang.more <gaurang.more@sunday.tech>
2024-09-29 21:33:39 +0530

    Product Detail: Created format for productDetailpage Data

---
+++ src/staticData/productDetailData.js
@@ -0,0 +1,122 @@
+export const printerDetails = {
+    "cwc-m5040dnw": {
+        title: "CWC M5040DN/W Multifunction Laser Printer",
+        specSummary: [
+            "Printing speed (A4): 43ppm",
+            "Connection method: Network, WiFi and USB",
+            "Basic function: Print, Copy, Scan",
+            "Automatic duplex printing",
+            "One-step driver installation",
+            "Silent printing, reduce noise",
+        ],
+        details: {
+            print_specification: [
+                {
+                    title: "Print speed",
+                    desc: "43 ppm",
+                },
+                {
+                    title: "First print out time",
+                    desc: "Less than 8 Seconds",
+                },
+                {
+                    title: "Duplex printing",
+                    desc: "Automatic",
+                },
+                {
+                    title: "Operating system compatibility",
+                    desc: " Windows： Server 2012/2016/2019/XP/Win7/Win8.1/Win10/Win11（32/64 bit）Linux(BOSS), Maya, Fedora.",
+                },
+            ],
+            paper_handling: [
+                {
+                    title: "Paper input tray",
+                    desc: "250 sheets"
+                },
+                {
+                    title: "Paper output tray",
+                    desc: "100 sheets"
+                },
+                {
+                    title: "Media sizes",
+                    desc: "A4, Letter, Legal, B5 (ISO), B5 (JIS), Executive,A5, Envelope - Monarch, 60~120g/m²"
+                },
+            ],
+            general: [
+                {
+                    title: "Paper input tray",
+                    desc: "250 sheets"
+                },
+                {
+                    title: "Processor",
+                    desc: "1.2GHz"
+                },
+                {
+                    title: "RAM",
+                    desc: "512MB"
+                },
+                {
+                    title: "Print Resolution",
+                    desc: "2400 x 1200 dpi"
+                },
+                {
+                    title: "Bypass Tray / ADF",
+                    desc: "YES"
+                },
+                {
+                    title: "Interface",
+                    desc: "Network, WiFi and USB port"
+                },
+                {
+                    title: "Power Supply",
+                    desc: "220-240V AC, ~50/60Hz (110V optional)"
+                },
+            ],
+            package_specification: [
+                {
+                    title: "Dimension (D×W×H)",
+                    desc: "44 x 36 x 42.3cm (WxDxH)"
+                },
+                {
+                    title: "Package size (D×W×H)",
+                    desc: "53.5 x 47.7 x 54.5 cm"
+                },
+                {
+                    title: "Product Weight (with cartridge)",
+                    desc: "14.3 kg"
+                },
+                {
+                    title: "Gross Weight ",
+                    desc: "17.14 kg"
+                },
+            ],
+            consumable: [
+                {
+                    title: "Consumable type",
+                    desc: "Separate drum and toner"
+                },
+                {
+                    title: "Toner Cartridge",
+                    desc: "CWC TE504S"
+                },
+                {
+                    title: "Drum Unit",
+                    desc: "CWC DM503"
+                }
+            ]
+
+        },
+    },
+    "cwc-m5030dnw": {},
+    "cwc-m2010dn/w": {},
+    "cwc-mb5020nw": {},
+    "cwc-m1005nw": {},
+
+    //LASER PRINTER BELOW
+    "cwc-p5040dnw": {},
+    "cwc-p5030dnw": {},
+    "cwc-p5010dnw": {},
+    "cwc-pb5040dnw": {},
+    "cwc-p5040": {},
+    "cwc-1020nw": {},
+};
