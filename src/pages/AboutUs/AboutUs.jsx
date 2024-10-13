b8d95e0df4324c61fe81691a71c2c10b0d24d25c
gaurang.more <gaurang.more@sunday.tech>
2024-10-13 22:42:19 +0530

    About Us : Ui Creation

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,11 +1,97 @@
-
+import { Divider } from "antd";
+import KeepInTouch from "../../commonComponents/KeepInTouch";
+import OurCertifications from "../../commonComponents/OurCertifications";
+import { aboutUs } from "../../staticData/aboutUs";
 
 const AboutUs = () => {
-    return (
-        <div>
-            About Us Page
-        </div>
-    )
-}
+  return (
+    <section>
+      {/* Banner Section  */}
+      <div className="w-full">
+        <img
+          src="/images/aboutUs/Banner.png"
+          alt="Banner"
+          className="w-full h-auto"
+        />
+      </div>
 
-export default AboutUs
+      {/* Company Profile  */}
+      <div className="w-[80%] mx-auto mb-20">
+        <Divider className="!my-20">
+          <p className="text-4xl font-bold px-10">Company Profile</p>
+        </Divider>
+
+        <div className="w-full flex">
+          <div className="w-[45%] flex flex-col justify-between p-4">
+            <p
+              className="text-[30px] font-bold mb-3"
+              dangerouslySetInnerHTML={{
+                __html: aboutUs.companyProfile.heading,
+              }}
+            ></p>
+            <p className="text-[25px] font-medium">
+              {aboutUs.companyProfile.desc}
+            </p>
+          </div>
+          <div className="w-[55%] border-2">
+            {/* <img src="" alt="Logo" /> */}
+            IMAGE LOGO GOES HERE
+          </div>
+        </div>
+      </div>
+
+      {/* image section 1st */}
+      <div className="w-[80%] mx-auto mb-20">
+        <img
+          src="images/aboutUs/about_1.png"
+          className="w-full h-auto"
+          alt="banner"
+        />
+      </div>
+
+      {/* mid info section  */}
+      <div className="w-[80%] mx-auto mb-20">
+        <p className="text-[30px] font-semibold text-justify">
+          {aboutUs?.info}
+        </p>
+      </div>
+
+      {/* image section 2nd */}
+      <div className="w-[80%] mx-auto mb-20">
+        <img
+          src="images/aboutUs/about_2.png"
+          className="w-full h-auto"
+          alt="banner"
+        />
+      </div>
+
+      {/* mid info section 2nd */}
+      <div className="w-[80%] mx-auto mb-20">
+        <p className="text-[30px] font-semibold text-justify">
+          {aboutUs?.info2}
+        </p>
+      </div>
+
+      {/* Image Section 3rd */}
+
+      <div className="w-[80%] mx-auto mb-20">
+
+        <div className="flex justify-between mb-[6%]">
+          <img src="images/aboutUs/mission.png" alt="our mission" className="w-[47%] h-auto"/>
+          <img src="images/aboutUs/vision.png" alt="our vision" className="w-[47%] h-auto"/>
+        </div>
+
+        <img src="images/aboutUs/aim.png" alt="aim" />
+        
+      </div>
+
+      {/* Certifications  */}
+      <OurCertifications />
+
+      {/* Keep In Touch  */}
+      <KeepInTouch />
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ src/staticData/aboutUs.js
@@ -0,0 +1,10 @@
+export const aboutUs = {
+    companyProfile : {
+        heading : "E-Compusell Private Limited:<br/> India's First Indigenous Laser Printer Manufacturer",
+        desc : "E-Compusell Private Limited is a dynamic and innovative company specializing in the design, development, and manufacturing of high-quality Make in India Laser Printers, Laser Multifunction Printers (MFPs) and consumables under the brand name CWC. Established in 2016, E-Compusell Private Limited has quickly gained recognition for its latest chip technology, reliable products, and exceptional customer service"
+    },
+
+    info : 'E-Compusell Private Limited is the company behind the CWC brand and we are addicted to upholding our commitment to innovation, reliability, and customer satisfaction. With our strong foundation in advanced technology and a passionate team, we are paised to make a significant impact in the printing industry',
+
+    info2 : "We are fully aware of our responsibility to the environment and the world’s limited resources. Our products are designed for long lifetimes and low energy consumption, contributing to the reduction of waste and conservation of raw materials. In line with the European Community’s Directive 2002/95/EC (RoHS), our products are free from lead, mercury, cadmium, and other harmful substances. This helps protect the environment and reduce pollution. The RoHS directive is crucial for manufacturers and suppliers, aiming to minimize hazardous substances in products, impacting both the ecosystem and everyday life."
+}
