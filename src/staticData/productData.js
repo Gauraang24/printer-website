f12409291ae9271e2e18d62112217f02d24de348
gaurang.more <gaurang.more@sunday.tech>
2025-01-12 20:25:25 +0530

    Products : Added New Printers (M235, M4843, M5028)

--- src/staticData/productData.js
+++ src/staticData/productData.js
@@ -1,116 +1,134 @@
 export const printersCollection = [
-    {
-        heading: "CWC Multifunction Laser Printer Series",
-        data: [
-            {
-                title: "CWC M5040DN/W",
-                link: "images/productPage/CWC_M1.png",
-                key: "M1",
-                navigate: "cwc-m5040dnw",
-            },
-            {
-                title: "CWC M5030DN/W",
-                link: "images/productPage/CWC_M2.png",
-                key: "M2",
-                navigate: "cwc-m5030dnw",
-            },
-            {
-                title: "CWC M2010DN/W",
-                link: "images/productPage/CWC_M3.png",
-                key: "M3",
-                navigate: "cwc-m2010dnw",
-            },
-            {
-                title: "CWC MB5020N/W",
-                link: "images/productPage/CWC_M4.png",
-                key: "M4",
-                navigate: "cwc-mb5020nw",
-            },
-            {
-                title: "CWC M1005N/W",
-                link: "images/productPage/CWC_M5.png",
-                key: "M5",
-                navigate: "cwc-m1005nw",
-            },
-        ],
-    },
-    {
-        heading: "CWC Laser Printer Series",
-        data: [
-            {
-                title: "CWC P5040DN/W",
-                link: "images/productPage/CWC_P1.png",
-                key: "P1",
-                navigate: "cwc-p5040dnw",
-            },
-            {
-                title: "CWC P5030DN/W",
-                link: "images/productPage/CWC_P2.png",
-                key: "P2",
-                navigate: "cwc-p5030dnw",
-            },
-            {
-                title: "CWC P5010DN/W",
-                link: "images/productPage/CWC_P3.png",
-                key: "P3",
-                navigate: "cwc-p5010dnw",
-            },
-            {
-                title: "CWC PB5040DN/W",
-                link: "images/productPage/CWC_P4.png",
-                key: "P4",
-                navigate: "cwc-pb5040dnw",
-            },
-            {
-                title: "CWC P5040",
-                link: "images/productPage/CWC_P5.png",
-                key: "P5",
-                navigate: "cwc-p5040",
-            },
-            {
-                title: "CWC 1020N/W",
-                link: "images/productPage/CWC_P6.png",
-                key: "P6",
-                navigate: "cwc-1020nw",
-            },
-        ],
-    },
+  {
+    heading: "CWC Multifunction Laser Printer Series",
+    data: [
+      {
+        title: "CWC M5040DN/W",
+        link: "images/productPage/CWC_M1.png",
+        key: "M1",
+        navigate: "cwc-m5040dnw",
+      },
+      {
+        title: "CWC M5030DN/W",
+        link: "images/productPage/CWC_M2.png",
+        key: "M2",
+        navigate: "cwc-m5030dnw",
+      },
+      {
+        title: "CWC M2010DN/W",
+        link: "images/productPage/CWC_M3.png",
+        key: "M3",
+        navigate: "cwc-m2010dnw",
+      },
+      {
+        title: "CWC MB5020N/W",
+        link: "images/productPage/CWC_M4.png",
+        key: "M4",
+        navigate: "cwc-mb5020nw",
+      },
+      {
+        title: "CWC M1005N/W",
+        link: "images/productPage/CWC_M5.png",
+        key: "M5",
+        navigate: "cwc-m1005nw",
+      },
+      {
+        title: "CWC M5028",
+        link: "images/productDetail/M5028/img1.webp",
+        key: "M6",
+        navigate: "cwc-m5028",
+      },
+      {
+        title: "CWC M4843",
+        link: "images/productDetail/M4843/img1.webp",
+        key: "M7",
+        navigate: "cwc-m4843",
+      },
+      {
+        title: "CWC M235",
+        link: "images//productDetail/M235/img1.webp",
+        key: "M8",
+        navigate: "cwc-m235",
+      },
+    ],
+  },
+  {
+    heading: "CWC Laser Printer Series",
+    data: [
+      {
+        title: "CWC P5040DN/W",
+        link: "images/productPage/CWC_P1.png",
+        key: "P1",
+        navigate: "cwc-p5040dnw",
+      },
+      {
+        title: "CWC P5030DN/W",
+        link: "images/productPage/CWC_P2.png",
+        key: "P2",
+        navigate: "cwc-p5030dnw",
+      },
+      {
+        title: "CWC P5010DN/W",
+        link: "images/productPage/CWC_P3.png",
+        key: "P3",
+        navigate: "cwc-p5010dnw",
+      },
+      {
+        title: "CWC PB5040DN/W",
+        link: "images/productPage/CWC_P4.png",
+        key: "P4",
+        navigate: "cwc-pb5040dnw",
+      },
+      {
+        title: "CWC P5040",
+        link: "images/productPage/CWC_P5.png",
+        key: "P5",
+        navigate: "cwc-p5040",
+      },
+      {
+        title: "CWC 1020N/W",
+        link: "images/productPage/CWC_P6.png",
+        key: "P6",
+        navigate: "cwc-1020nw",
+      },
+    ],
+  },
 ];
 
 export const cartridgeCollection = [
-    {
-        heading: "Cartridges",
-        data: [
-            {
-                title: "CWC TW502S",
-                link: "images/productPage/Cartridge_T1.png",
-                key: "T1",
-                navigate: "cwc-tw502S",
-            },
-            {
-                title: "CWC TE201L",
-                link: "images/productPage/Cartridge_T2.png",
-                key: "T2",
-                navigate: "cwc-te201l",
-            },
-            {
-                title: "CWC TE503S",
-                link: "images/productPage/Cartridge_T3.png",
-                key: "T3",
-                navigate: "cwc-te503s",
-            },
-            {
-                title: "CWC TE504S",
-                link: "images/productPage/Cartridge_T4.png",
-                key: "T4",
-                navigate: "cwc-te504s",
-            },
-            {
-                title: "CWC 102-2K",
-                link: "images/productPage/Cartridge_T5.png",
-                key: "T5",
-                navigate: "cwc-102-2k",
-            },
-        ],
-    },
+  {
+    heading: "Cartridges",
+    data: [
+      {
+        title: "CWC TW502S",
+        link: "images/productPage/Cartridge_T1.png",
+        key: "T1",
+        navigate: "cwc-tw502S",
+      },
+      {
+        title: "CWC TE201L",
+        link: "images/productPage/Cartridge_T2.png",
+        key: "T2",
+        navigate: "cwc-te201l",
+      },
+      {
+        title: "CWC TE503S",
+        link: "images/productPage/Cartridge_T3.png",
+        key: "T3",
+        navigate: "cwc-te503s",
+      },
+      {
+        title: "CWC TE504S",
+        link: "images/productPage/Cartridge_T4.png",
+        key: "T4",
+        navigate: "cwc-te504s",
+      },
+      {
+        title: "CWC 102-2K",
+        link: "images/productPage/Cartridge_T5.png",
+        key: "T5",
+        navigate: "cwc-102-2k",
+      },
+    ],
+  },
 ];
--- src/staticData/productDetailData.js
+++ src/staticData/productDetailData.js
@@ -425,6 +425,479 @@ export const printerDetails = {
     imgFolderName: "M1005NW",
   },
 
+  "cwc-m5028": {
+    title: "CWC M5028 Multifunction Laser Printer",
+    type: "printer",
+    specSummary: [
+      "Printing speed (A4): 28ppm",
+      "Connection method: Network, WiFi and USB",
+      "Print Technology: Laser",
+      "Automatic easy duplex printing",
+      "One-step driver installation",
+      "MAKE IN INDIA",
+    ],
+    imgFolderName: "M5028",
+    details: {
+      print_specification: [
+        {
+          title: "Print speed",
+          desc: "28 ppm",
+        },
+        {
+          title: "Monthly Duty Cycle",
+          desc: "50000*",
+        },
+        {
+          title: "Duplex printing",
+          desc: "Automatic",
+        },
+        {
+          title: "Operating system compatibility",
+          desc: " Windows： Server 2012/2016/2019/XP/Win7/Win8.1/Win10/Win11（32/64 bit) Linux(BOSS), Maya, Fedora.",
+        },
+      ],
+      paper_handling: [
+        {
+          title: "Paper input tray",
+          desc: "250 sheets",
+        },
+        {
+          title: "Paper output tray",
+          desc: "100 sheets",
+        },
+        {
+          title: "Bypass Tray Capacity",
+          desc: "100",
+        },
+        {
+          title: "Media sizes",
+          desc: "A4, Letter, Legal, B5 (ISO), B5 (JIS), Executive,A5, Envelope - Monarch, 60~120g/m²",
+        },
+      ],
+
+      Scan_specification: [
+        {
+          title: "Scan speed",
+          desc: "ADF : 17PPM",
+        },
+        {
+          title: "Copy speed",
+          desc: "ADF : 1. Multi pages copy-17ppm ; 2, Single page-23ppm",
+        },
+        {
+          title: "Scan resolution",
+          desc: "600 dpi (Max) 300 dpi(Default)",
+        },
+        {
+          title: "Feeder Capacity",
+          desc: "50 Pgs.",
+        },
+        {
+          title: "Max. scan size",
+          desc: "21.6 x 29.7cm",
+        },
+      ],
+
+      general: [
+        {
+          title: "Function",
+          desc: "Print, Scan & Copy",
+        },
+        {
+          title: "Processor",
+          desc: "1.2GHz",
+        },
+        {
+          title: "RAM",
+          desc: "512MB",
+        },
+        {
+          title: "Print Resolution",
+          desc: "1200 x 1200 dpi",
+        },
+        {
+          title: "Bypass Tray / ADF",
+          desc: "YES",
+        },
+        {
+          title: "Interface",
+          desc: "Network, WiFi and USB port",
+        },
+        {
+          title: "Power comsumption",
+          desc: "400W(printing), 3W(sleep Mode)",
+        },
+        {
+          title: "Power Supply",
+          desc: "220-240V AC, ~50/60Hz (110V optional)",
+        },
+        {
+          title: "Country Of Origin",
+          desc: "India",
+        },
+        {
+          title: "Warranty",
+          desc: "3 Years Standard Warranty",
+        },
+      ],
+      package_specification: [
+        {
+          title: "Dimension (D×W×H)",
+          desc: "40.5 x 33.5 x 36cm(WxDxH)",
+        },
+        {
+          title: "Package size (D×W×H)",
+          desc: "53.8 x 43.8 x 48 cm",
+        },
+        {
+          title: "Product Weight (with cartridge)",
+          desc: "11.7 kg",
+        },
+        {
+          title: "Gross Weight ",
+          desc: "13 kg",
+        },
+      ],
+      consumable: [
+        {
+          title: "Consumable type",
+          desc: "Separate drum and toner / Composite",
+        },
+        {
+          title: "Minimum Cartridge Yield(Starter)",
+          desc: "1500 Pgs.",
+        },
+        {
+          title: "Toner Cartridge",
+          desc: "CWC TE503S",
+        },
+        {
+          title: "Drum Unit",
+          desc: "CWC DM503",
+        },
+      ],
+    },
+  },
+  "cwc-m4843": {
+    title: "CWC M4843 Multifunction Laser Printer",
+    type: "printer",
+    imgFolderName: "M4843",
+    specSummary: [
+      "Printing speed (A4): 43ppm",
+      "Connection method: Network, WiFi and USB",
+      "Print Technology: Laser",
+      "Automatic easy duplex printing",
+      "One-step driver installation",
+      "MAKE IN INDIA",
+    ],
+    details: {
+      print_specification: [
+        {
+          title: "Print speed",
+          desc: "43 ppm",
+        },
+        {
+          title: "Monthly Duty Cycle",
+          desc: "100000*",
+        },
+        {
+          title: "Duplex printing",
+          desc: "Automatic",
+        },
+        {
+          title: "Operating system compatibility",
+          desc: " Windows： Server 2012/2016/2019/XP/Win7/Win8.1/Win10/Win11（32/64 bit) Linux(BOSS), Maya, Fedora.",
+        },
+      ],
+      paper_handling: [
+        {
+          title: "Paper input tray",
+          desc: "250 sheets",
+        },
+        {
+          title: "Paper output tray",
+          desc: "100 sheets",
+        },
+        {
+          title: "Bypass Tray Capacity",
+          desc: "100",
+        },
+        {
+          title: "Media sizes",
+          desc: "A4, Letter, Legal, B5 (ISO), B5 (JIS), Executive,A5, Envelope - Monarch, 60~120g/m²",
+        },
+      ],
+
+      Scan_specification: [
+        {
+          title: "Scan speed",
+          desc: "24 ppm(A4) / 25 ppm(Letter)",
+        },
+        {
+          title: "Copy speed",
+          desc: "40cpm(A4) / 42cpm(Letter)",
+        },
+        {
+          title: "Scan resolution",
+          desc: "600 dpi (Max) 300 dpi(Default)",
+        },
+        {
+          title: "Feeder type",
+          desc: "DADF/RADF",
+        },
+        {
+          title: "Feeder Capacity",
+          desc: "50 Pgs.",
+        },
+        {
+          title: "Max. scan size",
+          desc: "21.6 x 29.7cm",
+        },
+        {
+          title: "Output function",
+          desc: "Scan to E-mail, PC, FTP, Thumb drive, iOS/Android",
+        },
+      ],
+
+      general: [
+        {
+          title: "Function",
+          desc: "Print, Scan, Copy and Fax(Optional)",
+        },
+        {
+          title: "Processor",
+          desc: "1.2GHz",
+        },
+        {
+          title: "RAM",
+          desc: "512MB",
+        },
+        {
+          title: "Print Resolution",
+          desc: "1200 x 1200 dpi",
+        },
+        {
+          title: "Bypass Tray / ADF",
+          desc: "YES",
+        },
+        {
+          title: "Interface",
+          desc: "Network, WiFi and USB port",
+        },
+        {
+          title: "Power comsumption",
+          desc: "650W(printing), 1W(sleep Mode)",
+        },
+        {
+          title: "Power Supply",
+          desc: "220-240V AC, ~50/60Hz (110V optional)",
+        },
+        {
+          title: "Country Of Origin",
+          desc: "India",
+        },
+        {
+          title: "Warranty",
+          desc: "3 Years Standard Warranty",
+        },
+      ],
+      package_specification: [
+        {
+          title: "Dimension (D×W×H)",
+          desc: "41.5 x 36,5 x 37cm (WxDxH)",
+        },
+        {
+          title: "Package size (D×W×H)",
+          desc: "53.5 x 47.7 x 54.5 cm",
+        },
+        {
+          title: "Product Weight (with cartridge)",
+          desc: "12.5 kg",
+        },
+        {
+          title: "Gross Weight ",
+          desc: "15.14 kg",
+        },
+      ],
+      consumable: [
+        {
+          title: "Consumable type",
+          desc: "Separate drum and toner / Composite",
+        },
+        {
+          title: "Minimum Cartridge Yield(Starter)",
+          desc: "3000 Pgs.",
+        },
+        {
+          title: "Toner Cartridge",
+          desc: "CWC T4843",
+        },
+        {
+          title: "Drum Unit",
+          desc: "CWC D4843-30K",
+        },
+      ],
+    },
+  },
+  "cwc-m235": {
+    title: "CWC M235 Multifunction Laser Printer",
+
+    type: "printer",
+    specSummary: [
+      "Printing speed (A4): 35ppm",
+      "Connection method: Network, WiFi and USB",
+      "Print Technology: Laser",
+      "Automatic easy duplex printing",
+      "One-step driver installation",
+      "MAKE IN INDIA",
+    ],
+    details: {
+      print_specification: [
+        {
+          title: "Print speed",
+          desc: "35 ppm",
+        },
+        {
+          title: "Monthly Duty Cycle",
+          desc: "80000*",
+        },
+        {
+          title: "Duplex printing",
+          desc: "Automatic",
+        },
+        {
+          title: "Operating system compatibility",
+          desc: " Windows： Server 2012/2016/2019/XP/Win7/Win8.1/Win10/Win11（32/64 bit) Linux(BOSS), Maya, Fedora.",
+        },
+      ],
+      paper_handling: [
+        {
+          title: "Paper input tray",
+          desc: "250 sheets",
+        },
+        {
+          title: "Paper output tray",
+          desc: "100 sheets",
+        },
+        {
+          title: "Bypass Tray Capacity",
+          desc: "100",
+        },
+        {
+          title: "Media sizes",
+          desc: "A4, Letter, Legal, B5 (ISO), B5 (JIS), Executive,A5, Envelope - Monarch, 60~120g/m²",
+        },
+      ],
+
+      Scan_specification: [
+        {
+          title: "Scan speed",
+          desc: "24 ppm(A4) / 25 ppm(Letter)",
+        },
+        {
+          title: "Copy speed",
+          desc: "33cpm(A4) / 35cpm(Letter)",
+        },
+        {
+          title: "Scan resolution",
+          desc: "600 dpi (Max) 300 dpi(Default)",
+        },
+        {
+          title: "Feeder Capacity",
+          desc: "50 Pgs.",
+        },
+        {
+          title: "Max. scan size",
+          desc: "21.6 x 29.7cm",
+        },
+        {
+          title: "Output function",
+          desc: "Scan to E-mail, PC, FTP, Thumb drive, iOS/Android",
+        },
+      ],
+
+      general: [
+        {
+          title: "Function",
+          desc: "Print, Scan, Copy and Fax(Optional)",
+        },
+        {
+          title: "Processor",
+          desc: "525MHz",
+        },
+        {
+          title: "RAM",
+          desc: "256MB",
+        },
+        {
+          title: "Print Resolution",
+          desc: "1200 x 1200 dpi",
+        },
+        {
+          title: "Bypass Tray / ADF",
+          desc: "YES",
+        },
+        {
+          title: "Interface",
+          desc: "USB, Ethernet, Wi-Fi, Wi-Fi Direct, NFC",
+        },
+        {
+          title: "Power comsumption",
+          desc: "550W(printing), 2W(sleep Mode)",
+        },
+        {
+          title: "Power Supply",
+          desc: "220-240V AC, ~50/60Hz (110V optional)",
+        },
+        {
+          title: "Country Of Origin",
+          desc: "India",
+        },
+        {
+          title: "Warranty",
+          desc: "3 Years Standard Warranty",
+        },
+      ],
+      package_specification: [
+        {
+          title: "Dimension (D×W×H)",
+          desc: "41.5 x 36.5 x 350 (WxDxH)",
+        },
+        {
+          title: "Package size (D×W×H)",
+          desc: "47.5 x 39 x 35 cm",
+        },
+        {
+          title: "Product Weight (with cartridge)",
+          desc: "8.36 kg",
+        },
+        {
+          title: "Gross Weight ",
+          desc: "11.29 kg",
+        },
+      ],
+      consumable: [
+        {
+          title: "Consumable type",
+          desc: "Separate drum and toner / Composite",
+        },
+        {
+          title: "Minimum Cartridge Yield(Starter)",
+          desc: "3000 Pgs.",
+        },
+        {
+          title: "Toner Cartridge",
+          desc: "CWC T0235",
+        },
+        {
+          title: "Drum Unit",
+          desc: "CWC D0235-12K",
+        },
+      ],
+    },
+    imgFolderName: "M235",
+  },
+
   //LASER PRINTER BELOW
   "cwc-p5040dnw": {
     title: "CWC P5040DN/W Laser Printer",
