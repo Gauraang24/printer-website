af4da62ed67cfec784044c9f748899ac02d59026
gaurang.more <gaurang.more@sunday.tech>
2025-09-27 12:57:32 +0530

    Updated cwc-3336 details

--- src/staticData/productDetailData.js
+++ src/staticData/productDetailData.js
@@ -1922,7 +1922,7 @@ export const printerDetails = {
       consumable: [
         {
           title: "Consumable type",
-          desc: "Composite",
+          desc: "Separate Drum and Toner / Composite",
         },
         {
           title: "Minimum Cartridge Yield(Starter)",
@@ -1932,10 +1932,10 @@ export const printerDetails = {
           title: "Toner Cartridge",
           desc: "CWC-T310, CWC-T3333 & CWC-T3336",
         },
-        // {
-        //   title: "Drum Unit",
-        //   desc: "CWC-D310",
-        // },
+        {
+          title: "Drum Unit",
+          desc: "CWC-D310",
+        },
       ],
     },
   },
